fd0d49f8b178205752036c1cee646bb3b64f48a7
Sylvester Kwon <sylvesterkwon@gmail.com>
2025-01-20 20:59:41 +0900

    feat: Add AppBar, Footer

---
+++ src/app/components/AppBar.tsx
@@ -0,0 +1,80 @@
+import {
+  AppBar as MUIAppBar,
+  Button,
+  IconButton,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import Link from "next/link";
+import { Book } from "@mui/icons-material";
+
+export default function AppBar({}) {
+  return (
+    <MUIAppBar position="static">
+      <Toolbar>
+        <Stack
+          direction="row"
+          spacing={2}
+          justifyContent="space-between"
+          sx={{ flexGrow: 1 }}
+        >
+          <Stack direction="row" spacing={2} alignItems="center">
+            <Book />
+            <Link
+              href="/"
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <Typography
+                variant="h6"
+                style={{ color: "inherit", marginLeft: 8 }}
+              >
+                CP hub
+              </Typography>
+            </Link>
+            <Button
+              color="inherit"
+              sx={{ display: { xs: "none", md: "block" } }}
+            >
+              Problems
+            </Button>
+            <Button
+              color="inherit"
+              sx={{ display: { xs: "none", md: "block" } }}
+            >
+              Forum
+            </Button>
+          </Stack>
+          <Stack direction="row" spacing={2} alignItems="center">
+            <Button
+              size="small"
+              variant="outlined"
+              color="inherit"
+              sx={{ display: { xs: "none", md: "block" } }}
+            >
+              Login
+            </Button>
+            <Button
+              size="small"
+              variant="outlined"
+              color="inherit"
+              sx={{ display: { xs: "none", md: "block" } }}
+            >
+              Register
+            </Button>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Stack>
+        </Stack>
+      </Toolbar>
+    </MUIAppBar>
+  );
+}
---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export default function Footer({}) {
+  return (
+    <footer>
+      <Typography variant="body2" align="center" color="grey">
+        CP-Hub by SylvesterKwon
+      </Typography>
+    </footer>
+  );
+}
